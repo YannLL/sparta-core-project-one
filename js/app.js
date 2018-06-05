020064de31b85d7ed48cf0e5e0b8419d37b95d79
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-06-05 17:21:56 +0100

    frontal collisions between ball and players

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   //player
   var player1 = $('#player1');
   var player2 = $('#player2');
+  var p1object = {};
+  var p2object = {};
 
   //ball
   var ball = $('#ball');
@@ -10,25 +12,106 @@ $(document).ready(function(){
   var dirY = "+"; //+ is down, - is up
   var velocity = 1;
   var gravity = 1;
+  var ballObject = {};
 
   var start = $('.start');
   var gameFrame = $('#gameFrame');
 
+  //ENVIRONMENT
   var gameFrameTop = gameFrame.offset().top;
   var gameFrameBottom = gameFrameTop + gameFrame.height();
   var gameFrameLeft = gameFrame.offset().left;
   var gameFrameRight = gameFrameLeft + gameFrame.width();
+  //NET a
+  //NET b
 
   // END RESOURCES ==========
 
+  // ===== KEY BINDINGS =====
+  setInterval(movePlayers, 20);
+    var keys = {}
 
-  // ===== BALL PHYSICS =====
+    $(document).keydown(function(e) {
+        keys[e.keyCode] = true;
+    });
+
+    $(document).keyup(function(e) {
+        delete keys[e.keyCode];
+    });
+
+    function movePlayers() {
+        for (var direction in keys) {
+            if (!keys.hasOwnProperty(direction)) continue;
+            if (direction == 65) {
+                $(player1).animate({left: "-=8"}, 0);
+            }
+            if (direction == 68) {
+                $(player1).animate({left: "+=8"}, 0);
+            }
+            if (direction == 37) {
+                $(player2).animate({left: "-=8"}, 0);
+            }
+            if (direction == 39) {
+                $(player2).animate({left: "+=8"}, 0);
+            }
+        }
+    };// END KEY BINDINGS ==========
+
+
+  // ===== GAME PHYSICS =====
   setInterval(function(){
 
+    //BALL VARIABLES
     var ballTop = ball.offset().top;
     var ballBottom = ballTop + ball.height();
     var ballLeft = ball.offset().left;
     var ballRight = ballLeft + ball.width();
+    ballObject.left = ballLeft;
+    ballObject.right = ballRight;
+    ballObject.top = ballTop;
+    ballObject.bottom = ballBottom;
+
+    // console.log(ballObject);
+
+
+    //PLAYER VARIABLES
+    //PLAYER1
+    var p1Top = player1.offset().top;
+    var p1Bottom = p1Top + player1.height();
+    var p1Left = player1.offset().left;
+    var p1Right = p1Left + player1.width();
+
+    p1object.left = p1Left;
+    p1object.right = p1Right;
+    p1object.top = p1Top;
+    p1object.bottom = p1Bottom;
+
+    //PLAYER2
+    var p2Top = player2.offset().top;
+    var p2Bottom = p2Top + player2.height();
+    var p2Left = player2.offset().left;
+    var p2Right = p2Left + player2.width();
+
+    p2object.left = p2Left;
+    p2object.right = p2Right;
+    p2object.top = p2Top;
+    p2object.bottom = p2Bottom;
+
+
+    //PLAYER COLLISION
+    //Player1Collisions
+    if(ballObject.left < p1object.right &&
+      ballObject.bottom > p1object.top &&
+      ballObject.right < p1object.top){
+      dirX = "+"
+    };
+
+    //Player2Collisions
+    if(ballObject.right > p2object.left &&
+      ballObject.bottom > p2object.top
+      ){
+      dirX = "-"
+    };
 
     // GRAVITY
     //IF gravity is above -5, run a for loop drecrementing gravity back to -5
@@ -49,15 +132,12 @@ $(document).ready(function(){
     if(dirY === "+"){
       ball.css("top",gravity);
       gravity ++
-      console.log(ballBottom);
     };
 
     if(dirY === "-"){
       ball.css("top",gravity)
       gravity--;
       };
-
-
 
     //Frame box collisions
     if(ballRight > gameFrameRight){
@@ -77,7 +157,6 @@ $(document).ready(function(){
       console.log("ceiling bounce =]");
       dirY = "+";
     }
-
 
   }, 10);
 
